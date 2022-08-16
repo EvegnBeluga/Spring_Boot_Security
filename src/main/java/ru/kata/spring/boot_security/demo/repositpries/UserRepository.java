be6a084c79784453348838a6a75b1a59cc936334
Artem311k <ovtrue311k@gmail.com>
2022-08-16 21:29:17 +0300

    Edit FetchType from Eager to Fetch

--- src/main/java/ru/kata/spring/boot_security/demo/repositpries/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositpries/UserRepository.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.repositpries;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.kata.spring.boot_security.demo.models.User;
 
 import java.util.Optional;
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query(value = "select u from User u join fetch u.roles where u.email=?1")
     Optional<User> findByEmail(String email);
 }
